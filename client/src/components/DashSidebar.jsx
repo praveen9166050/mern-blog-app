6b28a406b59b7e7dc299ba086db7bc9d9bf458da
Praveen Kumar <praveen9166050@gmail.com>
2024-08-02 21:50:49 +0530

    Added posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,13 +1,14 @@
 import { Sidebar } from "flowbite-react"
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi"
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi"
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { signInFailure, signoutSuccess } from "../redux/user/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState('');
+  const {currentUser} = useSelector((state) => state.user);
   const dispatch = useDispatch();
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -32,12 +33,27 @@ function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
-            <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label="User" labelColor="dark" >
+            <Sidebar.Item 
+              as='div' 
+              active={tab === 'profile'} 
+              icon={HiUser} 
+              label={currentUser.isAdmin ? "Admin" : "User"} 
+              labelColor="dark" 
+            >
               Profile
             </Sidebar.Item>
           </Link>
+          {
+            currentUser.isAdmin && (
+              <Link to="/dashboard?tab=posts">
+                <Sidebar.Item as='div' active={tab === 'posts'} icon={HiDocumentText} label="" labelColor="">
+                  Posts
+                </Sidebar.Item>
+              </Link>
+            )
+          }
           <Sidebar.Item icon={HiArrowSmRight} onClick={handleSignout} className="cursor-pointer" >
             Sign Out
           </Sidebar.Item>
